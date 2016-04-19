8541f52e54b3d79df45393134a8e0e485dccf193
Nathan Walker <walkerrunpdx@gmail.com>
2016-04-18 17:37:38 -0700

    fix demo to run correctly

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
-import {ParallaxPage } from "./app.component";
-// the compliler complains when trying to import nativescript-parallax directly, below commented out g.
-// import {ParallaxViewCommon as ParallaxView, Header, Content, Anchored} from 'nativescript-parallax/nativescript-parallax.common';
-import {ParallaxView, Header, Content, Anchored} from 'nativescript-parallax';
-import { registerElement } from 'nativescript-angular/element-registry';
-
-registerElement('Content', () => Content);
-registerElement('Header', () => Header);
-registerElement('Anchored', () => Anchored);
-registerElement('Parallax', () => ParallaxView);
-
+import {ParallaxPage} from "./app.component";
 
 nativeScriptBootstrap(ParallaxPage);
 
